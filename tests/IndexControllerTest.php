5946c948e2c6bf3539dfc5cea02133f559c71405
Yevgeniy Dyachenko <yevgeniy.dyachenko@gmail.com>
2017-01-13 17:28:59 +0200

    add tests

---
+++ tests/IndexControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Controller\IndexController;
+use Framework\Bootstrap\Router;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DomCrawler\Crawler;
+
+
+class IndexControllerTest extends TestCase
+{
+
+
+    public function testIndexActionFunctional()
+    {
+        $readerController = new IndexController();
+        ob_start();
+        $readerController->index();
+        $result = ob_get_clean();
+
+        $crawler = new Crawler($result);
+
+
+        $this->assertEquals('Home', $crawler->filter('title')->text());
+    }
+
+}
